dc582b03d4da8b6baeb8976a0241c398ab05e7f9
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-12-15 14:14:00 +0500

    models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    size = models.IntegerField()
+    country = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name},{self.category}'
+
