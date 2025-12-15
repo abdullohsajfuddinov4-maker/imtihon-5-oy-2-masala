e80b32da3584486df00129b0405e8cdf7e2bfa54
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-12-15 14:03:52 +0500

    settings

---
+++ Oyoqkiyim_doon/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Oyoqkiyim_doon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Oyoqkiyim_doon.settings')
+
+application = get_asgi_application()
---
+++ Oyoqkiyim_doon/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for Oyoqkiyim_doon project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'Oyoqkiyim_doon.settings'
+)
+
+application = get_wsgi_application()
+
