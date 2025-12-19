5a37a22500573561bbf446614fd2f51960eebf0a
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-12-19 16:10:56 +0500

    ProfileUpdateView

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,41 @@ from django import forms
 from .models import CustomUser
 
 class CustomUserRegisterForm(forms.ModelForm):
+    password1 = forms.CharField(label='password',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
     class Meta:
         model = CustomUser
-        fields = ['username','first_name','last_name','email','image','address','password']
+        fields = ['username','first_name','last_name','email','image','address',]
+
+    def clean(self):
+        data = super().clean()
+        if data.get('password1') != data.get('password2') :
+            raise forms.ValidationError('password mas emas')
+        return date
+
+
+class CustomUserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username','first_name','last_name','email','image','address',]
+
+
+
+class CustomUserChangePasswordForm(forms.ModelForm):
+    old_password = forms.CharField(label='password',widget=forms.PasswordInput)
+    new_password = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
+    confirm_new_password = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
+
+    def clean(self):
+        date = super().clean()
+        if self.data['new_password'] != self.data['confirm_new_password']:
+            raise forms.ValidationError('Yangi prollar mas emas')
+        return date
+
+    def save(self, commit = True):
+        user = super().save(commit=False)
+        if user.check_password(self.cleaned_data['old_password']):
+             raise forms.ValidationError('Eski parol hato')
+        user.set_password(self.cleaned_data['new_password'])
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
+from pyexpat.errors import messages
 
-from .forms import CustomUserRegisterForm
+from .forms import CustomUserRegisterForm ,CustomUserUpdateForm,CustomUserChangePasswordForm
 
 
 class RegisterView(View):
@@ -14,7 +15,12 @@ class RegisterView(View):
     def post(self, request):
         form = CustomUserRegisterForm(request.POST, request.FILES)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+
+            password = form.cleaned_data.get('password1')
+            user.set_password(password)
+
+            user.save()
             login(request, user)
             return redirect('home')
 
@@ -50,3 +56,18 @@ class ProfileView(LoginRequiredMixin, View):
 
     def get(self, request):
         return render(request, 'user/profile.html')
+
+
+class ProfileUpdateView(View):
+    def get(self,request):
+        form = CustomUserUpdateForm(instance=request.user)
+        return render(request,'user/profile_uodate.html',{'form':form})
+
+    def post(self,request):
+        form = CustomUserUpdateForm(request.POST , request.FILES, instance=request.user)
+        if form.is_valid():
+            user = form.save()
+            redirect('profile',pk=user.pk)
+        return render(request,'user/profile_uodate.html',{'form':form})
+
+
